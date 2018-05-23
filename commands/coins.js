a811ad3fbdefc58d55e45eb54d9089ec3d4f88f5
iHaxtedU <38415651+iHaxtedU@users.noreply.github.com>
2018-05-23 16:45:13 -0500

    Update coins.js

--- commands/coins.js
+++ commands/coins.js
@@ -20,7 +20,12 @@ let coinEmbed = new Discord.RichEmbed()
 if(!user1) message.channel.send(coinEmbed).then(msg => {msg.delete(5000)})
   
 if(user1){
-let ccoins = coins[user1.id].coins;
+let ccoins = coins[user1.user.id].coins;
+if(!coins[user1.user.id]){
+  coins[user1.user.id] = {
+    coins: 100
+  };
+}
 message.channel.send(ccoins);
 }
 };
