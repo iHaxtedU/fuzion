a7417b8f0b456cb364c2888fd555d82eb997c0e8
iHaxtedU <38415651+iHaxtedU@users.noreply.github.com>
2018-05-23 16:41:53 -0500

    Update coins.js

--- commands/coins.js
+++ commands/coins.js
@@ -20,23 +20,9 @@ let coinEmbed = new Discord.RichEmbed()
 if(!user1) message.channel.send(coinEmbed).then(msg => {msg.delete(5000)})
   
 if(user1){
-let usercoins = coins[user1].coins;
-  
-  if(!coins[user1]){
-  coins[user1] = {
-    coins: 100
-  };
-}
-
-let usercoin1s = new Discord.RichEmbed()
-.setAuthor(bot.user.username, bot.user.avatarURL)
-.setColor("#4286f4")
-.addField("Here Is The Information You Requested", `${user1.username} Has ${usercoins} FUZCoins`);
- 
-message.channel.send(usercoin1s).then(msg => {msg.delete(5000)})
-}
-
-}
+let ccoins = coins[user1.id].coins;
+message.channel.send(ccoins);
+};
 
 module.exports.help = {
   name: "coins"
