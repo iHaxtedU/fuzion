dca1fda22dd8116a999ad4db2b74de2c669213ac
iHaxtedU <38415651+iHaxtedU@users.noreply.github.com>
2018-05-23 16:50:24 -0500

    Update coins.js

--- commands/coins.js
+++ commands/coins.js
@@ -10,8 +10,8 @@ if(!coins[message.author.id]){
   
 let user1 = message.mentions.users.first()
 let ccoins = coins[user1.id].coins;
-if(!coins[user1.id]){
-  coins[user1.id] = {
+if(!ccoins[user1.id]){
+  ccoins[user1.id] = {
     coins: 100
   };
 }
