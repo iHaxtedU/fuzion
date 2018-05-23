dc76ef1bc83089c57112c67d6ec2c4a348ed9929
iHaxtedU <38415651+iHaxtedU@users.noreply.github.com>
2018-05-23 16:29:37 -0500

    Update coins.js

--- commands/coins.js
+++ commands/coins.js
@@ -7,29 +7,24 @@ if(!coins[message.author.id]){
     coins: 0
   };
 }
-
+  
 let user1 = message.mentions.users.first()
+if(!user1) message.channel.send(coinEmbed).then(msg => {msg.delete(5000)})
+let uCoins = coins[message.author.id].coins;
+  
+let coinEmbed = new Discord.RichEmbed()
+.setAuthor(bot.user.username, bot.user.avatarURL)
+.setColor("00FF00")
+.addField("You Have", `${uCoins} FUZCoins`);
+
 let usercoins = coins[user1.id].bal;
 
 let usercoin1s = new Discord.RichEmbed()
 .setAuthor(bot.user.username, bot.user.avatarURL)
 .setColor("#4286f4")
 .addField("Here Is The Information You Requested", `${user1.username} Has ${usercoins} FUZCoins`);
-  
-if(user1){
+ 
 message.channel.send(usercoin1s).then(msg => {msg.delete(5000)})
-}
-  
-let uCoins = coins[message.author.id].coins;
-
-let coinEmbed = new Discord.RichEmbed()
-.setAuthor(bot.user.username, bot.user.avatarURL)
-.setColor("00FF00")
-.addField("You Have", `${uCoins} FUZCoins`);
-
-if(!user1) {
-message.channel.send(coinEmbed).then(msg => {msg.delete(5000)})
-}
 
 }
 
