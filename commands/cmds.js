66a0292dbd96fe328bef1dda7c93dbd68768547a
iHaxtedU <38415651+iHaxtedU@users.noreply.github.com>
2018-05-24 14:45:32 -0500

    Update cmds.js

--- commands/cmds.js
+++ commands/cmds.js
@@ -5,8 +5,8 @@ let help = new Discord.RichEmbed()
 .setAuthor(bot.user.username, bot.user.avatarURL)
 .setColor("#42ebf4")
 .addField(">ping, "**Used To Check The Response Time Of The Bot")
-.addField(">coins", "**Check How Many FUZCoins You Have")
-.addField(">pay", "**Pay Someone An Ammount Of FUZCoins");
+.addField(">coins", "**Check How Many FUZCoins You Have**")
+.addField(">pay", "**Pay Someone An Ammount Of FUZCoins**");
 
 message.channel.send(help);
 }
